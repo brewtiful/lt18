7764a6d441502e87ddb24e1b1ec0677a3ecd3a5d
brewtiful <39200460+brewtiful@users.noreply.github.com>
2018-05-16 09:08:56 -0700

    update 20.valid-parentheses.cpp

---
+++ 20.valid-parentheses.cpp
@@ -0,0 +1,76 @@
+/*
+ * [20] Valid Parentheses
+ *
+ * https://leetcode.com/problems/valid-parentheses/description/
+ *
+ * algorithms
+ * Easy (34.08%)
+ * Total Accepted:    336.4K
+ * Total Submissions: 986.8K
+ * Testcase Example:  '"()"'
+ *
+ * Given a string containing just the characters '(', ')', '{', '}', '[' and
+ * ']', determine if the input string is valid.
+ * 
+ * An input string is valid if:
+ * 
+ * 
+ * Open brackets must be closed by the same type of brackets.
+ * Open brackets must be closed in the correct order.
+ * 
+ * 
+ * Note that an empty string is also considered valid.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: "()"
+ * Output: true
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: "()[]{}"
+ * Output: true
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: "(]"
+ * Output: false
+ * 
+ * 
+ * Example 4:
+ * 
+ * 
+ * Input: "([)]"
+ * Output: false
+ * 
+ * 
+ * Example 5:
+ * 
+ * 
+ * Input: "{[]}"
+ * Output: true
+ * 
+ * 
+ */
+class Solution {
+public:
+    bool isValid(string s) {
+	stack<char> st;
+	int n = s.size();
+	if (!n) return true;
+	unordered_map<char, char> um;
+	um[')'] = '(';
+	um[']'] = '[';
+	um['}'] = '{';
+	for (auto c : s) {
+		if (st.size() && um[c] == st.top()) st.pop();
+		else st.push(c);
+	}    
+	return st.empty();
+    }
+};
